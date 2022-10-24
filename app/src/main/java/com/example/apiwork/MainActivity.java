e38dc47bf837b5287114b888fcdc0b5ac4d0b257
MKudryash <Jukudryash@gmail.com>
2022-10-24 15:33:30 +0300

    Update MainActivity.java
    changes Url

--- app/src/main/java/com/example/apiwork/MainActivity.java
+++ app/src/main/java/com/example/apiwork/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Void... voids) {
             try {
-                URL url = new URL("http://192.168.10.176:80/NGKNN/МамшеваЮС/api/Products");//Строка подключения к нашей API
+                URL url = new URL("http://ssfb.ngknn.local:80/NGKNN/МамшеваЮС/api/Products");//Строка подключения к нашей API
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //вызываем нашу API
 
                 BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
